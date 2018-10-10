6890cc7e2da5bc6b7fa9dc223124cf7718e7ef2d
JaccoVE <jaccovanekris@me.com>
2018-10-10 19:35:17 +0200

    Updated for Linux support

--- R_Files/DataScience_Project3.R
+++ R_Files/DataScience_Project3.R
@@ -8,12 +8,23 @@ library(xml2)
 library(data.table)
 library(gtools)
 
-# Load meta data
-meta_data_main <- data.table::fread(file = "D:/DataScienceData/Data/NDW/utwente snelheden groot amsterdam 1 dag met metadata 20160916T105028 197/utwente snelheden groot amsterdam  1 dag met metadata_snelheid_00001.csv")
-
-# Load data of one day
-data_main <- data.table::fread(file = "D:/DataScienceData/Data/NDW/utwente snelheden groot amsterdam/utwente snelheden groot amsterdam _snelheid_00001.csv",
-                               nrows = 7898604)
+if(.Platform$OS.type == "unix") {
+    # Load meta data
+    meta_data_main <- data.table::fread(file = "/media/jacco/HDD/DataScienceData/Data/NDW/utwente snelheden groot amsterdam 1 dag met metadata 20160916T105028 197/utwente snelheden groot amsterdam  1 dag met metadata_snelheid_00001.csv",
+                                        nThread = getDTthreads())
+  
+    # Load data of one day
+    data_main <- data.table::fread(file = "/media/jacco/HDD/DataScienceData/Data/NDW/utwente snelheden groot amsterdam/utwente snelheden groot amsterdam _snelheid_00001.csv",
+                                   nrows = 7898604,
+                                   nThread = getDTthreads())
+} else {
+    # Load meta data
+    meta_data_main <- data.table::fread(file = "D:/DataScienceData/Data/NDW/utwente snelheden groot amsterdam 1 dag met metadata 20160916T105028 197/utwente snelheden groot amsterdam  1 dag met metadata_snelheid_00001.csv")
+  
+    # Load data of one day
+    data_main <- data.table::fread(file = "D:/DataScienceData/Data/NDW/utwente snelheden groot amsterdam/utwente snelheden groot amsterdam _snelheid_00001.csv",
+                                   nrows = 7898604)
+}
 
 # Generate msiteUniqueInfo table
 msiteUniqueInfo <- meta_data_main %>%
