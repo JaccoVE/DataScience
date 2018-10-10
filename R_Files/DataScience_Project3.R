fa72853eb73da1de1310235127a920a0fdecfa3d
Jacco <jaccovanekris@me.com>
2018-10-10 17:40:10 +0200

    Faster way of loading files and now generates two tables of the starscheme

--- R_Files/DataScience_Project3.R
+++ R_Files/DataScience_Project3.R
@@ -5,47 +5,55 @@ library(readr)
 library(dplyr)
 library(lubridate)
 library(xml2)
+library(data.table)
+library(gtools)
 
 # Load meta data
-meta_data_main <- read.csv(file = "D:/DataScienceData/Data/NDW/utwente snelheden groot amsterdam 1 dag met metadata 20160916T105028 197/utwente snelheden groot amsterdam  1 dag met metadata_snelheid_00001.csv", nrows = 10000)
+meta_data_main <- data.table::fread(file = "D:/DataScienceData/Data/NDW/utwente snelheden groot amsterdam 1 dag met metadata 20160916T105028 197/utwente snelheden groot amsterdam  1 dag met metadata_snelheid_00001.csv")
 
-# Select required columns from meta data
-meta_data_main <- meta_data_main %>%
+# Load data of one day
+data_main <- data.table::fread(file = "D:/DataScienceData/Data/NDW/utwente snelheden groot amsterdam/utwente snelheden groot amsterdam _snelheid_00001.csv",
+                               nrows = 7898604)
+
+# Generate msiteUniqueInfo table
+msiteUniqueInfo <- meta_data_main %>%
   select(
-      "measurementSiteReference",
-      "measurementSiteVersion",
-      "index",
-      "computationMethod",
-      "measurementSiteName1",
-      "measurementSiteNumberOfLanes",
-      "measurementSide",
-      "specificLane",
-      "specificVehicleCharacteristics",
-      "startLocatieForDisplayLat",
-      "startLocatieForDisplayLong",
-      "specificLocation") %>%
-  arrange(
-      "measurementSiteReference",
-      "index") %>%
+    "measurementSiteReference",
+    "generatedSiteName",
+    "startLocatieForDisplayLat",
+    "startLocatieForDisplayLong",
+    "specificLocation",
+    "alertCDirectionCoded") %>%
+  arrange(measurementSiteReference) %>%
   group_by(
-      "measurementSiteReference",
-      "index") %>%
+    measurementSiteReference) %>%
   distinct() %>%
-  ungroup() %>%
+  ungroup()
 
-# Load data
-data_main <- read.csv(file = "D:/DataScienceData/Data/NDW/utwente snelheden groot amsterdam/utwente snelheden groot amsterdam _snelheid_00001.csv", nrows = 10000)
+# Generate index table
+indexUniqueInfo <- meta_data_main %>%
+  select("index",
+         "specificLane",
+         "specificVehicleCharacteristics") %>%
+  arrange(
+        index) %>%
+  group_by(
+        index) %>%
+  distinct() %>%
+  ungroup()
 
-# Select required columns from data
-data_main <- data_main %>%
-  select("measurementSiteReference",
-         "index",
-         "periodStart",
-         "periodEnd",
-         "numberOfIncompleteInputs",
-         "avgVehicleSpeed",
-         "generatedSiteName"
-  )
+# Tableau table for Duncan-chan witch coordinates
+coordinates <- meta_data_main %>%
+  select("startLocatieForDisplayLat",
+         "startLocatieForDisplayLong") %>%
+  arrange(
+    startLocatieForDisplayLat) %>%
+  group_by(
+    startLocatieForDisplayLat) %>%
+  distinct() %>%
+  ungroup()
+
+data.table::fwrite(coordinates, file = "coordinates.csv")
 
 # Join meta data and data
 data_main <- data_main %>%
