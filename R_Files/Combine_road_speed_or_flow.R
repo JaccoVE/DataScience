1a6619944d23133de375852d0cf6a18d34217a39
JaccoVE <jaccovanekris@me.com>
2018-10-31 17:36:33 +0100

    Changed filename

--- R_Files/Combine_road_speed_or_flow.R
+++ R_Files/Combine_road_speed_or_flow.R
@@ -21,12 +21,15 @@ registerDoMC(4)
 sep_symbol <- ","
 
 # Folder Locations
-f_main <- "/home/jacco/Documents/Git/DataScience/NDW/Tableau Input Files/"
+f_NDW <- "/home/jacco/Documents/Git/DataScience/NDW/Tableau Input Files/"
+f_Status <- "/home/jacco/Documents/Git/DataScience/Status/Tableau Input Files/"
 
 f_input <- c("intensity_data_PerDay", 
              "intensity_data_PerHour",
              "speed_data_PerDay", 
-             "speed_data_PerHour")
+             "speed_data_PerHour",
+             "road_data_Amsterdam_PerDay.csv",
+             "road_data_Amsterdam_PerHour.csv")
 
 f_output <- "/home/jacco/Documents/Git/DataScience/NDW/Tableau Input Files/"
 
@@ -37,12 +40,12 @@ f_output <- "/home/jacco/Documents/Git/DataScience/NDW/Tableau Input Files/"
 gc()
 
 # Iterate over each file in parallel
-foreach(i=1:length(f_input)) %dopar% {
+foreach(i=1:4) %dopar% {
   
   i <- 1
 
   # Load meta data intensity
-  data = data.table::fread(file = paste(f_main, f_input[i], ".csv", sep="", collapse=NULL),
+  data = data.table::fread(file = paste(f_NDW, f_input[i], ".csv", sep="", collapse=NULL),
                                           nThread = 1)
   
   # Collect garbage
@@ -54,28 +57,26 @@ foreach(i=1:length(f_input)) %dopar% {
       week_day = weekdays(as.Date(data$date,'%Y-%m-%d')))
   
   # Handle difference in flow and speed column names
-  if(grepl(f_input[i], "intensity"))
+  if(i == 1 | i == 2) # Flow 
   {
-    print("speed")
+    # Add reference column
+    data = data %>%
+      mutate(reference = ave(data$avg_flow, data$week_day))
+    
+    # Add difference column
+    data_test = data %>%
+      mutate(difference = data$avg_flow - data$reference)
+    
+    # Save to csv file
+    data.table::fwrite(road_data,
+                       nThread = 1,
+                       file = paste(f_output, f_input[i], "2.csv", sep="", collapse=NULL),
+                       sep = sep_symbol)
   }
-  else
+  else # Speed
   {
-    print("flow")
+    
   }
-  
-  # Add reference column
-  data = data %>%
-    mutate(reference = ave(data$avg_flow, data$week_day))
-  
-  # Add difference column
-  data_test = data %>%
-    mutate(difference = data$avg_flow - data$reference)
-  
-  # Save to csv file
-  data.table::fwrite(road_data,
-                     nThread = 1,
-                     file = paste(f_output, f_input[i], "2.csv", sep="", collapse=NULL),
-                     sep = sep_symbol)
   
   # Collect garbage
   gc()
