b30ac60fcbdd250f59805472d4e5e10da0ba8d61
JaccoVE <jaccovanekris@me.com>
2018-11-01 17:02:20 +0100

    Location should now do what it has to do

--- Database/convertToExcel.sh
+++ Database/convertToExcel.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-for file in dataFlow/*
-do
-	unoconv --format xls "$file"
-done
+#for file in dataFlow/*
+#do
+#	unoconv --format xls "$file"
+#done
 
-for file in dataSpeed/*
-do
-	unoconv --format xls "$file"
-done
+#for file in dataSpeed/*
+#do
+#	unoconv --format xls "$file"
+#done
 
 unoconv --format xls "dataRoad.csv"
 unoconv --format xls "Location.csv"
