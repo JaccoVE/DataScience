081587f5f09db32073e1f0858c61d1311b3bc924
JaccoVE <jaccovanekris@me.com>
2018-11-01 16:38:06 +0100

    Added location table

--- Database/convertToExcel.sh
+++ Database/convertToExcel.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-#for file in dataFlow/*
-#do
-#	unoconv --format xls "$file"
-#done
+for file in dataFlow/*
+do
+	unoconv --format xls "$file"
+done
 
-#for file in dataSpeed/*
-#do
-#	unoconv --format xls "$file"
-#done
+for file in dataSpeed/*
+do
+	unoconv --format xls "$file"
+done
 
 unoconv --format xls "dataRoad.csv"
+unoconv --format xls "Location.csv"
 unoconv --format xls "metaFlow.csv"
 unoconv --format xls "metaRoad.csv"
 unoconv --format xls "metaSpeed.csv"
