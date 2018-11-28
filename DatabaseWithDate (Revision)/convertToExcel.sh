5bd3a7fb4490b2e4fc02bf9b5b3298f7ff8be476
JaccoVE <jaccovanekris@me.com>
2018-11-28 12:06:04 +0100

    Solved an error (minus to plus) and coverted to excel files

--- DatabaseWithDate (Revision)/convertToExcel.sh
+++ DatabaseWithDate (Revision)/convertToExcel.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#for file in dataFlow/*
-#do
-#	unoconv --format xls "$file"
-#done
+for file in dataFlow/*
+do
+	unoconv --format xls "$file"
+done
 
-#for file in dataSpeed/*
-#do
-#	unoconv --format xls "$file"
-#done
+for file in dataSpeed/*
+do
+	unoconv --format xls "$file"
+done
 
 for file in dataRoad/*
 do
